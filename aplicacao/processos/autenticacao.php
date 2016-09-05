b03dc83031fd0aeb7c033b95984fc5c2940db40e
Gustavo <gugatxr@gmail.com>
2016-09-05 20:23:28 -0300

    Branchs criadas

--- aplicacao/processos/autenticacao.php
+++ aplicacao/processos/autenticacao.php
@@ -5,23 +5,27 @@
   $vUsuario = $_POST['usuario'];
 
   //Consulta sql para validar o login.
-    $resultado = $bd->query("SELECT nome, id_permissoes FROM imobiliaria.usuarios WHERE usuario = '$vUsuario' AND senha = '$vSenha' ");
+    $resultado = $bd->query("SELECT usuario, id_permissoes
+      FROM imobiliaria.usuarios WHERE usuario = '$vUsuario' AND senha = '$vSenha'");
   //@var int resultado
   //rowCount retorna o numero de linhas do resultado da consulta
-    $resultado = $resultado->rowCount();
+    $numero_usuarios = $resultado->rowCount();
   // se retornar 1 linha para ser valido
   // para 0 não ha registro no banco de Dados
   //se retornar mais de 1 precisa ser revisado o banco de dados
-  if($resultado === 1)
+  if($numero_usuarios === 1)
   {
     echo "Logado com sucesso!";
-
-  }elseif($resultado === 0)
+    if(session_id() == '' || !isset($_SESSION))
+    {
+      // session isn't started
+      $nome_usuario = $resultado->fetch(PDO::FETCH_ASSOC);
+      $nome_usuario = $nome_usuario['usuario'];
+      session_start();
+      $_SESSION["$nome_usuario"] = $nome_usuario;
+    }
+  }elseif($numero_usuarios === 0)
   {
     echo "Dados incorretos";
-    // if(session_id() == '' || !isset($_SESSION))
-    // {
-    //   // session isn't started
-    //   session_start();
-    // }
   }
+  echo $_SESSION['admin'];
