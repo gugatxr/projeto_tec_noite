33915c5f5844f11f261b19e279872109f532b6ab
francine <fbpa.hollweg@gmail.com>
2016-09-28 21:30:51 -0300

    css

--- aplicacao/template/menu.php
+++ aplicacao/template/menu.php
@@ -4,13 +4,18 @@
 		<meta charset="utf-8">
 		<title> menu</title>
 		<link rel="stylesheet" type="text/css" href="../css/style.css" />
-
+	
 	</head>
 	<body>
-
+		
 		<ul class="nav">
 
 			<li><a href="#">usuario</a></li>
+			<ul>
+					<li><b href="#">editar</a></li>
+			
+			
+			</ul>
 			<li><a href="#">usuario</a></li>
 			<li><a href="#">usuario</a></li>
 			<li><a href="#">usuario</a></li>
